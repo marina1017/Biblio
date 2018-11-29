ba078dc4066e266324265f9f6445545181480a29
Marina Nakagawa <mari10172001@yahoo.co.jp>
2018-11-30 00:32:19 +0900

    tableViewの導入

--- Biblio/BookListViewController.swift
+++ Biblio/BookListViewController.swift
@@ -12,15 +12,40 @@ import SnapKit
 
 class BookListViewController: UIViewController {
 
-    var bannerView: GADBannerView!
+    var bannerView: GADBannerView = {
+        let bannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        return bannerView
+    }()
+    var tableView: UITableView!
+
+    let items = ["Apple","Banana","Orange"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        bannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        //広告バナー
         addBannerViewToView(bannerView)
-        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
         bannerView.rootViewController = self
         bannerView.load(GADRequest())
+
+        //テーブルビュー
+        self.tableView = {
+            let tableView = UITableView(frame: .zero, style: .plain)
+            self.view.addSubview(tableView)
+            return tableView
+        }()
+
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+
+        self.tableView.snp.makeConstraints{ make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide)
+            make.left.equalTo(self.view.safeAreaLayoutGuide)
+            make.right.equalTo(self.view.safeAreaLayoutGuide)
+            make.bottom.equalTo(self.bannerView.snp.top)
+        }
+
+
 
     }
 
@@ -36,3 +61,29 @@ class BookListViewController: UIViewController {
 
 }
 
+extension BookListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected! \(self.items[indexPath.row])")
+    }
+
+}
+
+extension BookListViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+            ?? UITableViewCell(style: .default, reuseIdentifier: "Cell")
+
+        cell.textLabel?.text = self.items[indexPath.row]
+
+        return cell
+    }
+}
